cb9ce15d5697685b573a65ffc5d16c28c6b5dca0
harsh <harshgkanjariya2468@gmail.com>
2021-01-30 09:36:51 +0530

    changes

--- app/src/main/java/com/harsh/easylife/ExperimentActivity.java
+++ app/src/main/java/com/harsh/easylife/ExperimentActivity.java
@@ -1,117 +1,110 @@
 package com.harsh.easylife;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
-import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.Filter;
-import android.widget.Filterable;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import com.harsh.hkutils.ExpandableLayout;
-import com.harsh.hkutils.HttpProcess;
-import com.harsh.hkutils.list.HKList;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
+import com.harsh.hkutils.calendar.EventCalendarView;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Locale;
+import java.util.Objects;
 
 public class ExperimentActivity extends AppCompatActivity {
 	private static final String TAG = "ExperimentActivity";
-	ArrayList<Map<String, String>> spinnerlist = new ArrayList<>();
-
-	String[] randomSuggestions = {"a", "aa", "ab", "aab", "abc", "abcd", "abcde", "abcdef"};
-	String[] tags = {"Java", "JavaScript", "Spring", "Java EE", "Java 8", "Java 9", "Java 10", "SQL", "SQLite"};
-	ArrayList<String> mOriginalValues = new ArrayList<>();
-
-	ArrayList<String> permissionsList = new ArrayList<>();
-	int expanded = -1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_experiment);
-		mOriginalValues.add("abc");
-		mOriginalValues.add("xyz");
-		mOriginalValues.add("xbz");
 
 		EventCalendarView calendarView=findViewById(R.id.calendar);
-		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-		String current = dateFormat.format(System.currentTimeMillis());
-		calendarView.setCallback(new EventCalendarView.Callback() {
-			@Override
-			public void onMonthSelect(int month) {
-				calendarView.ShowYearSelector();
-			}
-
-			@Override
-			public void onYearSelect(int year) {
-				calendarView.ShowDateSelector();
-
-			}
-		});
-
-		findViewById(R.id.root_calender_layout).setOnClickListener(view ->calendarView.ShowDateSelector());
+//		calendarView.setCallback(new EventCalendarView.Callback() {
+//			@Override
+//			public void onMonthSelect(int month) {
+//				calendarView.showYearSelector();
+//			}
+//			@Override
+//			public void onYearSelect(int year) {
+//				calendarView.showDateSelector();
+//			}
+//		});
+		findViewById(R.id.root_calender_layout).setOnClickListener(view ->calendarView.showDateSelector());
 
 		ColorStateList colorStateList=new ColorStateList(new int[][]{
 				new int[]{android.R.attr.state_selected},
-				new int[]{-android.R.attr.state_selected}
+				new int[]{-android.R.attr.state_selected},
+				new int[]{R.attr.state_present},
+				new int[]{R.attr.state_absent},
+				new int[]{R.attr.state_short_day},
+				new int[]{R.attr.state_half_day},
+				new int[]{R.attr.state_dynamic1_day},
+				new int[]{R.attr.state_dynamic2_day}
 		},new int[]{
-				Color.RED,
-				Color.WHITE
+				ContextCompat.getColor(this,R.color.colorPrimary),
+				Color.WHITE,
+				ContextCompat.getColor(this,R.color.success),
+				ContextCompat.getColor(this,R.color.error),
+				ContextCompat.getColor(this,R.color.short_day),
+				ContextCompat.getColor(this,R.color.half_day),
+				ContextCompat.getColor(this,R.color.dynamic1_day),
+				ContextCompat.getColor(this,R.color.dynamic2_day)
 		});
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy",Locale.US);
+
+		Calendar minCalendar = Calendar.getInstance();
+		minCalendar.set(Calendar.DAY_OF_MONTH,1);
+		try {
+			minCalendar.setTime(Objects.requireNonNull(dateFormat.parse(dateFormat.format(minCalendar.getTime()))));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		Calendar maxCalendar = Calendar.getInstance();
+		maxCalendar.set(Calendar.DAY_OF_MONTH,5);
+		maxCalendar.add(Calendar.MONTH,1);
+		try {
+			maxCalendar.setTime(Objects.requireNonNull(dateFormat.parse(dateFormat.format(maxCalendar.getTime()))));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		ArrayList<String>presentDates = new ArrayList<>();
+		presentDates.add("01/01/2021");
+		presentDates.add("05/01/2021");
+		ArrayList<String>absentDates = new ArrayList<>();
+		absentDates.add("14/01/2021");
+
 		calendarView.setInflater((calendar, layout, type) -> {
-			View v=layout.findViewById(R.id.dot);
+			ImageView v=layout.findViewById(R.id.dot);
 			if (type==EventCalendarView.DAY) {
 				v.setBackgroundTintList(colorStateList);
+				if (calendar.getTimeInMillis() < minCalendar.getTimeInMillis() ||
+						calendar.getTimeInMillis() > maxCalendar.getTimeInMillis()){
+					layout.setEnabled(false);
+					layout.setAlpha(.4f);
+				}
+				boolean found = false;
+				if (presentDates.contains(dateFormat.format(calendar.getTime()))) {
+					v.setColorFilter(ContextCompat.getColor(this,R.color.success));
+					found = true;
+				}
+				if (absentDates.contains(dateFormat.format(calendar.getTime()))) {
+					v.setColorFilter(ContextCompat.getColor(this,R.color.error));
+					found = true;
+				}
+				if (!found)
+					v.setVisibility(View.GONE);
 			}else{
 				v.setVisibility(View.GONE);
 			}
 		});
-
-		HttpProcess process = new HttpProcess("http://harsh.netwebdevelopers.com/add_tutorial.php");
-//		HttpProcess process=new HttpProcess("http://192.168.43.228/check.php");
-
-		Map<String, String> map = new HashMap<>();
-		map.put("test", "vvaall");
-//		Map<String,String>head=new HashMap<>();
-//		head.put("myhead","myval");
-
-		process.post(new HttpProcess.Callback() {
-			@Override
-			public void onError(IOException exception) {
-				Log.e(TAG, "onError: " + exception.getMessage());
-			}
-
-			@Override
-			public void onResponse(JSONObject json) throws JSONException {
-				Log.e(TAG, "onResponse: " + json);
-			}
-		}, map);
 	}
-	public void toggle(View view) {
-
-	}
+	public void toggle(View view) { }
 }
